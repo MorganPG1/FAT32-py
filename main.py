f40b2a077359375229ee9f6e26cb4cbf52f8c6c3
MorganPG1 <morganpgcontact@gmail.com>
2026-03-24 22:16:55 +0000

    add MBR partition decoders and a main.py for testing

---
+++ main.py
@@ -0,0 +1,24 @@
+from block.image import ImageFile
+from mbr.MBR import MBR_Decoder
+
+img = ImageFile("./disk.img")
+mbr = MBR_Decoder(img)
+
+signature = mbr.signature.hex()
+print(f"MBR Signature is: {signature}")
+if signature.lower() == "55aa":
+    print("Valid signature!")
+else:
+    print("Error: Signature is invalid, does this image have an MBR?")
+    exit()
+
+partitions = mbr.partitions
+print(f"This disk has {len(partitions)} partition(s)\n")
+
+for i, part in enumerate(partitions):
+    print(f"Partition {i+1}: ")
+    print(f"Size: {part.size} bytes")
+    print(f"Start position: {hex(part.start)}")
+    print(f"Type: {hex(part.type)}")
+    print(f"Attributes: {hex(part.attributes)}")
+    print()
--- mbr/MBR.py
+++ mbr/MBR.py
@@ -10,14 +10,22 @@ https://wiki.osdev.org/MBR_(x86)
 #Imports
 from block.blockDev import BlockDev
 
+class MBR_Partition():
+    def __init__(self, data:bytearray) -> None:
+        self.attributes = data[0]
+        self.type = int.from_bytes(data[0x4:0x5], "little")
+        self.start = int.from_bytes(data[0x8:0xC], "little") * 512
+        self.size = int.from_bytes(data[0xC:0x10], "little") * 512
 class MBR_Decoder():
     def __init__(self, disk:BlockDev) -> None:
         self.bootstrap = bytearray(disk.read(0x000, 440))
         self.diskId = bytearray(disk.read(0x1B8, 4))
-        self.part1Entry = bytearray(disk.read(0x1BE,16))
-        self.part2Entry = bytearray(disk.read(0x1CE,16))
-        self.part3Entry = bytearray(disk.read(0x1DE,16))
-        self.part4Entry = bytearray(disk.read(0x1EE,16))
+        self.partitionEntries = [bytearray(disk.read(0x1BE,16)), bytearray(disk.read(0x1CE,16)), bytearray(disk.read(0x1DE,16)), bytearray(disk.read(0x1EE,16))]
         self.signature = bytearray(disk.read(0x1FE,2))
         
+        self.partitions:list[MBR_Partition] = []
+        for entry in self.partitionEntries:
+            part = MBR_Partition(entry)
+            if part.type != 0:
+                self.partitions.append(part)
         pass

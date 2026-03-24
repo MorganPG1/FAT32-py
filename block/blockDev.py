4899e9968e2c34c0b955c1cc059d0c28b59f4e34
MorganPG1 <morganpgcontact@gmail.com>
2026-03-24 21:10:26 +0000

    Add a generic block device class

---
+++ block/blockDev.py
@@ -0,0 +1,39 @@
+'''
+FAT32-py - A FAT32 toolset written in python
+
+block/blockDev.py - MorganPG1:
+Generic block device class and methods
+'''
+
+class BlockDev():
+    def __init__(self) -> None:
+        '''
+        Generic block device class, inherit this in a block device implementation
+        
+        :param self: The BlockDev object
+        '''
+        pass
+    def read(self, offset:int, count:int) -> bytes:
+        '''
+        Reads data from the block device
+
+        :param self: The BlockDev object
+        :param offset: The offset to begin reading from
+        :type offset: int
+        :param count: The number of bytes to read
+        :type count: int
+        :return: The bytes read
+        :rtype: bytes
+        '''
+        return bytearray(count)
+    def write(self, offset:int, data:bytes) -> None:
+        '''
+        Writes data to the block device
+
+        :param self: The BlockDev object
+        :param offset: The offset to begin writing from
+        :type offset: int
+        :param data: The bytes to write
+        :type data: bytes
+        '''
+        pass
